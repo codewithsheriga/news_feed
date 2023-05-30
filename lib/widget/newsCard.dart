393fc2192f8dee077fe39b0b87ef1276ca672e18
Sherigabia <orionsavvy@gmail.com>
2023-05-30 14:56:34 +0000

    complete code

---
+++ lib/widget/newsCard.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class NewsCard extends StatelessWidget {
+  final String title;
+  final String description;
+  final String publishedAt;
+  final String author;
+  final String imageUrl;
+  const NewsCard(
+      {super.key,
+      required this.title,
+      required this.description,
+      required this.publishedAt,
+      required this.author,
+      required this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
+      width: double.infinity,
+      height: 300,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white54,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: const EdgeInsets.all(10),
+            width: double.infinity,
+            height: 150,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                    image: NetworkImage(imageUrl), fit: BoxFit.cover)),
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 20, right: 20, top: 5),
+            child: Column(children: [
+              Text(
+                title,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(fontSize: 20),
+              ),
+              Text(
+                description,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.person_rounded,
+                    color: Colors.black26,
+                  ),
+                  Text(author),
+                  Spacer(),
+                  Icon(
+                    Icons.calendar_month_outlined,
+                    color: Colors.black26,
+                  ),
+                  Text(
+                    publishedAt,
+                    style: TextStyle(color: Colors.black, fontSize: 10),
+                  )
+                ],
+              )
+            ]),
+          )
+        ],
+      ),
+    );
+  }
+}
